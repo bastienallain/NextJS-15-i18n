23d7d0ab8fa104ccaa5616975f98fd4c56060d5e
elevaseo <75243836+bastienallain@users.noreply.github.com>
2025-04-01 08:37:24 +0700

    Fix

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
-import {NextConfig} from 'next';
+import type {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin({
-  experimental: {
-    createMessagesDeclaration: './messages/en.json'
-  }
-});
+const withNextIntl = createNextIntlPlugin({});
 
-const config: NextConfig = {};
+const config: NextConfig = {
+  // Add any additional Next.js configuration here
+};
 
 export default withNextIntl(config);
